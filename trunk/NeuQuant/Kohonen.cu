83b3e67727ec0a6f2dc123cd0e90fb75f284b41f
dbottisti@gmail.com <dbottisti@gmail.com@2b572c62-0111-8fae-fbdf-7ecc799a8f30>
2011-11-18 02:42:49 +0000

    Final updates to CuNeuQuant algorithm.

--- trunk/NeuQuant/Kohonen.cu
+++ trunk/NeuQuant/Kohonen.cu
@@ -1,51 +1,223 @@
 /*
- * Kohonen.cpp
+ * Kohonen.cu
  *
- *  Created on: Oct 1, 2011
+ *  Created on: Oct 29, 2011
  *      Author: Dave
  */
 
 #include "Kohonen.h"
-#include "Utilities.h"
 #include <stdexcept>
-#include <thrust/copy.h>
-#include <thrust/host_vector.h>
+#include <cstdlib>
+#include <cmath>
+#include <cuda_runtime.h>
 
-//------------------------------------------------------------------------------
-// Name:        Kohonen()
-//------------------------------------------------------------------------------
-Kohonen::Kohonen(const unsigned int numInputDimensions,
-    const unsigned int numOutputDimensions,
-    const unsigned int * const networkSize,
-    const Utilities::NodeDistance& nodeDistance) :
-    numInputDimensions(numInputDimensions),
-    numOutputDimensions(numOutputDimensions), networkSize(NULL),
-    prodNetworkSize(1), nodeDistance(nodeDistance), randomInitialization(true)
+const unsigned int Kohonen::numInputDimensions = 3;
+const unsigned int Kohonen::networkSize = 256;
+
+// four primes near 500 - assume no image has a length so large
+// that it is divisible by all four primes
+const unsigned short Kohonen::primes[4] = {499, 491, 487, 503};
+
+// Device Code
+__shared__ float dist[Kohonen::networkSize];
+__shared__ unsigned char bestIdx[Kohonen::networkSize];
+__device__ float networkRed[Kohonen::networkSize];
+__device__ float networkGreen[Kohonen::networkSize];
+__device__ float networkBlue[Kohonen::networkSize];
+
+__global__ void initializeNetwork(void)
 {
-  this->networkSize = new unsigned int[numOutputDimensions];
-  memcpy(this->networkSize, networkSize,
-      sizeof(unsigned int) * numOutputDimensions);
+  const unsigned int idx = threadIdx.x;
+  networkRed[idx] = idx;
+  networkGreen[idx] = idx;
+  networkBlue[idx] = idx;
+}
 
-  for (unsigned int i = 0; i < numOutputDimensions; ++i)
-    prodNetworkSize *= networkSize[i];
+__global__ void trainOnPoint(const float * const points,
+    const unsigned int step, const unsigned int pointsPerIteration)
+{
+  const unsigned int numInputPoints = gridDim.x * gridDim.y;
+  const unsigned int tempPointIdx = blockIdx.y * gridDim.x + blockIdx.x;
+  const unsigned int pointIdx = (tempPointIdx * step) % numInputPoints;
+  const unsigned int networkIdx = threadIdx.x;
+  const unsigned int iteration = tempPointIdx / pointsPerIteration;
+  const float alpha = exp(-0.03 * iteration);
+  const unsigned int radius = static_cast<unsigned int>(
+      32.0 * exp(-0.0325 * iteration));
 
-  network.resize(prodNetworkSize * numOutputDimensions);
+  __shared__ Kohonen::Color point;
+  if (networkIdx == 0)
+  {
+    point.red = points[pointIdx];
+    point.green = points[pointIdx + numInputPoints];
+    point.blue = points[pointIdx + 2 * numInputPoints];
+  }
+  __syncthreads();
+
+  // Compute the distance between the training point and the network
+  dist[networkIdx] = fabs(point.red - networkRed[networkIdx]) +
+      fabs(point.green - networkGreen[networkIdx]) +
+      fabs(point.blue - networkBlue[networkIdx]);
+  bestIdx[networkIdx] = networkIdx;
+
+  // Wait for all the threads to compute their distance
+  __syncthreads();
+
+  // Reduce the distances
+  for (unsigned int s = blockDim.x >> 1; s > 0; s >>= 1)
+  {
+    if (networkIdx < s && dist[networkIdx + s] < dist[networkIdx])
+    {
+      dist[networkIdx] = dist[networkIdx + s];
+      bestIdx[networkIdx] = bestIdx[networkIdx + s];
+    }
+
+    __syncthreads();
+  }
+  const unsigned int winner = bestIdx[0];
+
+  // Update the node based on the contents of the winning node, if it is within
+  // the radius.
+  const unsigned int dist = (winner < networkIdx ?
+      networkIdx - winner : winner - networkIdx);
+  if (dist < radius)
+  {
+    // 1) Compute alpha * rho based upon distance to winning index
+    const float distOverRadius = static_cast<float>(dist) / radius;
+    const float alphaRho = alpha * (1 - distOverRadius * distOverRadius);
+    const float oneMinusAlphaRho = 1 - alphaRho;
+
+    // 2) Compute the new node
+    networkRed[networkIdx] = alphaRho * point.red +
+        oneMinusAlphaRho * networkRed[networkIdx];
+    networkGreen[networkIdx] = alphaRho * point.green +
+        oneMinusAlphaRho * networkGreen[networkIdx];
+    networkBlue[networkIdx] = alphaRho * point.blue +
+        oneMinusAlphaRho * networkBlue[networkIdx];
+  }
+}
+
+__global__ void mapPoints(float * const points)
+{
+  const unsigned int numInputPoints = gridDim.x * gridDim.y;
+  const unsigned int pointIdx = blockIdx.y * gridDim.x + blockIdx.x;
+  const unsigned int networkIdx = threadIdx.x;
+
+  __shared__ Kohonen::Color point;
+  if (networkIdx == 0)
+  {
+    point.red = points[pointIdx];
+    point.green = points[pointIdx + numInputPoints];
+    point.blue = points[pointIdx + 2 * numInputPoints];
+  }
+  __syncthreads();
+
+  // Compute the distance between the image point and the network
+  dist[networkIdx] = fabs(point.red - networkRed[networkIdx]) +
+      fabs(point.green - networkGreen[networkIdx]) +
+      fabs(point.blue - networkBlue[networkIdx]);
+  bestIdx[networkIdx] = networkIdx;
+
+  // Wait for all the threads in a block to compute their distance
+  __syncthreads();
+
+  // Reduce the distances by block
+  for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1)
+  {
+    if (networkIdx < s && dist[networkIdx + s] < dist[networkIdx])
+    {
+      dist[networkIdx] = dist[networkIdx + s];
+      bestIdx[networkIdx] = bestIdx[networkIdx + s];
+    }
+
+    __syncthreads();
+  }
+
+  // Copy the winning node to the output
+  if (networkIdx == 0)
+  {
+    const unsigned int winningIdx = bestIdx[0];
+    points[pointIdx] = networkRed[winningIdx];
+    points[pointIdx + numInputPoints] = networkGreen[winningIdx];
+    points[pointIdx + 2 * numInputPoints] = networkBlue[winningIdx];
+  }
+}
+
+Kohonen::Kohonen(void)
+{
+  // Initialize the network
+  initializeNetwork<<<1, networkSize>>>();
 }
 
 Kohonen::~Kohonen()
 {
-  if (networkSize)
-    delete [] networkSize;
+//  cudaFree(network);
 }
 
-//------------------------------------------------------------------------------
-// Name:        initialize
-//------------------------------------------------------------------------------
-void Kohonen::initialize(const float * const nodes)
+void Kohonen::train(const unsigned int width, const unsigned int height,
+    float * const pointsHost)
 {
-  randomInitialization = false;
+  const unsigned int numInputPoints = width * height;
+  const unsigned int pointsPerIteration = numInputPoints / 100;
+  cudaError_t status;
 
-  thrust::copy(nodes, nodes + prodNetworkSize * numOutputDimensions,
-      network.begin());
+  // Copy the image to the GPU
+  float *points;
+  cudaMalloc((void**)&points,
+      sizeof(float) * numInputPoints * numInputDimensions);
+  status = cudaGetLastError();
+  if (status != cudaSuccess)
+    throw std::runtime_error(std::string("CUDA Error: ") +
+        cudaGetErrorString(status));
+
+  cudaMemcpy(points, pointsHost,
+      sizeof(float) * numInputPoints * numInputDimensions,
+      cudaMemcpyHostToDevice);
+  status = cudaGetLastError();
+  if (status != cudaSuccess)
+    throw std::runtime_error(std::string("CUDA Error: ") +
+        cudaGetErrorString(status));
+
+  // Pick a prime number close to 500 that is not a factor of the number of
+  // pixels.  This is a step size to use for quasi-random training of the data
+  unsigned int step = primes[3];
+  for (unsigned int i = 0; i < 3; ++i)
+  {
+    if (numInputPoints % primes[i] != 0)
+    {
+      step = primes[i];
+      break;
+    }
+  }
+
+  dim3 gridDim(width, height, 1);
+
+  // Train the network
+  trainOnPoint<<<gridDim, networkSize>>>(points, step, pointsPerIteration);
+  status = cudaGetLastError();
+  if (status != cudaSuccess)
+    throw std::runtime_error(std::string("CUDA Error: ") +
+        cudaGetErrorString(status));
+
+  // Map the points to the output
+  mapPoints<<<gridDim, networkSize>>>(points);
+  status = cudaGetLastError();
+  if (status != cudaSuccess)
+    throw std::runtime_error(std::string("CUDA Error: ") +
+        cudaGetErrorString(status));
+
+  // Copy the result back to the host
+  cudaMemcpy(pointsHost, points,
+      sizeof(float) * numInputPoints * numInputDimensions,
+      cudaMemcpyDeviceToHost);
+  status = cudaGetLastError();
+  if (status != cudaSuccess)
+    throw std::runtime_error(std::string("CUDA Error: ") +
+        cudaGetErrorString(status));
+
+  cudaFree(points);
+  status = cudaGetLastError();
+  if (status != cudaSuccess)
+    throw std::runtime_error(std::string("CUDA Error: ") +
+        cudaGetErrorString(status));
 }
-
--- trunk/NeuQuant/Kohonen.h
+++ trunk/NeuQuant/Kohonen.h
@@ -1,189 +1,47 @@
 /*
  * Kohonen.h
  *
- *  Created on: Oct 1, 2011
+ *  Created on: Oct 29, 2011
  *      Author: Dave
  */
 
 #ifndef KOHONEN_H_
 #define KOHONEN_H_
 
-#include "Utilities.h"
-
 #include <math.h>
-#include <cstdlib>
 #include <thrust/functional.h>
 #include <thrust/device_vector.h>
-#include <thrust/iterator/discard_iterator.h>
 
 //------------------------------------------------------------------------------
 // Class:       Kohonen
-// Description: A class to train an arbitrary dimensional Kohonen Neural
-//              Network given training points
+// Description: A class to train a 1-dimensions Kohonen Neural Network given
+//              3-dimensional training points
 //------------------------------------------------------------------------------
 class Kohonen
 {
+  // member types
+public:
+  struct Color
+  {
+    float red;
+    float green;
+    float blue;
+  };
+
   // member methods
 public:
-  //----------------------------------------------------------------------------
-  // Name:        Kohonen
-  // Description: Constructor
-  // Parameters:  numInputDimensions - The number of dimensions of the input
-  //                training data
-  //              numOutputDimensions - The number of dimensions of the network.
-  //                This is usually less than numInputDimensions
-  //              networkSize - An array of numOutputDimensions elements, each
-  //                of which indicates the size of the network space in the
-  //                corresponding dimension.  I.e., the i'th item in
-  //                networkSize is the size of the i'th dimension of the
-  //                network space
-  //              nodeDistance - A pointer to a functor used to compute the
-  //                distances between nodes.  If specified, it is used instead
-  //                of the default.  If not specified but numOutputDimensions
-  //                is greater than 3, a runtime_error is thrown.
-  //                (Default: NULL, i.e., use default)
-  // Returns:     N/A
-  // Notes:       None
-  //----------------------------------------------------------------------------
-  Kohonen(const unsigned int numInputDimensions,
-      const unsigned int numOutputDimensions,
-      const unsigned int * const networkSize,
-      const Utilities::NodeDistance& nodeDistance = Utilities::nodeDistance1d);
+  Kohonen(void);
 
-  //----------------------------------------------------------------------------
-  // Name:        ~Kohonen
-  // Description: Destructor
-  // Parameters:  N/A
-  // Returns:     N/A
-  // Notes:       None
-  //----------------------------------------------------------------------------
+  void train(const unsigned int width, const unsigned int height,
+      float * const pointsHost);
+
   virtual ~Kohonen();
 
-  //----------------------------------------------------------------------------
-  // Name:        initialize
-  // Description: Initializes the neural network with the given nodes
-  // Parameters:  nodes - An array of product(networkSize) rows and
-  //                numOutputDimension columns, in row-major order.  These are
-  //                the nodes use to initialize the network.
-  // Returns:     N/A
-  // Notes:       - Initialization is optional.  If not performed, the network
-  //                is initialized to random points.
-  //              - If performed, and if relying on the built in node-distance
-  //                functions, the nodes should be specified in extended
-  //                row-major order.  That is, a linear scan of the nodes
-  //                should scan across the first dimension (rows) followed by
-  //                the second dimension (columns), then the third (slice), etc.
-  //----------------------------------------------------------------------------
-  void initialize(const float * const nodes);
-
-  //----------------------------------------------------------------------------
-  // Name:        train
-  // Description: Trains the neural network on the points.  Either sequential or
-  //                random order training may be selected
-  // Parameters:  points - An array of numPoints rows and numInputDimensions
-  //                columns, in row-major order.  These are the points to train.
-  //              numPoints - The number of points
-  //              numIterations - The number of iterations of training to
-  //                perform
-  //              randomize - True if the points should be shuffled randomly
-  //                before each iteration, false otherwise. (Default: FALSE)
-  // Returns:     N/A
-  // Notes:       None
-  //----------------------------------------------------------------------------
-  template <typename WeightDifferenceOperator,
-    typename WeightReductionOperator>
-  void train(const float * const points, const unsigned int numPoints,
-      const unsigned int numIterations, const bool randomize = false)
-  {
-    if (randomInitialization)
-    {
-      // Generate initialization on host
-      thrust::host_vector<float> initialHost(
-          prodNetworkSize * numOutputDimensions);
-      thrust::generate(initialHost.begin(), initialHost.end(), rand);
-
-      // Copy to device
-      thrust::copy(initialHost.begin(), initialHost.end(), network.begin());
-    }
-
-    // Copy the points to the device
-    thrust::device_vector<float> pointsDevice(points,
-        points + numPoints * numInputDimensions),
-        differences(numPoints * numInputDimensions), distances(numPoints);
-
-    for (unsigned int i = 0; i < numIterations; ++i)
-    {
-      // Loop through all of the points
-      thrust::device_vector<float>::const_iterator currentPoint =
-          pointsDevice.begin();
-      for (unsigned int j = 0; j < numPoints;
-          ++j, currentPoint += numInputDimensions)
-      {
-        // Compute the point-by-point difference using WeightDifferenceOperator
-        thrust::transform(pointsDevice.begin(), pointsDevice.end(),
-            thrust::make_permutation_iterator(
-                currentPoint,
-                thrust::make_transform_iterator(
-                    thrust::make_counting_iterator(0u),
-                    Utilities::ModConstant<unsigned int>(numInputDimensions))),
-            differences.begin(),
-            WeightDifferenceOperator());
-
-        // Reduce each row into a single distance using WeightReductionOperator
-        thrust::reduce_by_key(
-            thrust::make_transform_iterator(thrust::make_counting_iterator(0u),
-                Utilities::DivideConstant<unsigned int>(numInputDimensions)),
-            thrust::make_transform_iterator(thrust::make_counting_iterator(0u),
-                Utilities::DivideConstant<unsigned int>(numInputDimensions)) +
-                differences.size(),
-            differences.begin(),
-            thrust::make_discard_iterator(),
-            distances.begin(),
-            thrust::equal_to<unsigned int>(),
-            WeightReductionOperator());
-
-        // Find the winner (i.e., node with closest weight)
-        thrust::tuple<float, unsigned int> winner = thrust::min_element(
-            thrust::make_zip_iterator(
-                thrust::make_tuple(distances.begin(),
-                    thrust::make_counting_iterator(0u))),
-            thrust::make_zip_iterator(
-                thrust::make_tuple(distances.begin(),
-                    thrust::make_counting_iterator(0u))) + distances.size());
-        const unsigned int winnerIdx = thrust::get<1>(winner);
-
-        // Update the weights based upon the node distance to the winner
-      }
-    }
-
-  }
-
-  //----------------------------------------------------------------------------
-  // Name:        getPoints
-  // Description: Determine the mapping from the input points to points in the
-  //                neural network
-  // Parameters:  points - An array of numPoints rows and numInputDimensions
-  //                columns, in row-major order.  The input points to map.
-  //              mapped [output] - A pre-allocated array the same size as
-  //                points.  This method populates mapped with the
-  //                weights associated with the closest node to each point in
-  //                points.  In-place mapping is supported when mapped == NULL
-  //                (Default: mapped = NULL, i.e., in-place)
-  // Returns:     N/A
-  // Notes:       None
-  //----------------------------------------------------------------------------
-  void getPoints(const float * const points, float * const mapped = NULL);
-
   // member variables
-private:
-  const unsigned int numInputDimensions;
-  const unsigned int numOutputDimensions;
-  unsigned int *networkSize;
-  unsigned int prodNetworkSize;
-  const Utilities::NodeDistance &nodeDistance;
-
-  thrust::device_vector<float> network;
-  bool randomInitialization;
+public:
+  static const unsigned int numInputDimensions;
+  static const unsigned int networkSize;
+  static const unsigned short primes[4];
 };
 
-#endif /* KOHONEN_H_ */
+#endif /* KOHONEN2_H_ */
